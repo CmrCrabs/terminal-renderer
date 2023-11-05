025c5bc30a5fb071e05ea8a51fef98c868d912ca
CmrCrabs <zayaanazam1234@hotmail.com>
2023-11-05 22:23:25 +0000

    lines be working

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,12 @@ fn main() {
     let mut map: Vec<Vec<char>>;
 
     loop {
-        sleep(Duration::from_millis(17));
+        sleep(Duration::from_millis(1));
         (x, y) = get_dimensions();
         vertices = convert_to_clipspace(x, y, initial_vertices);
         map = create_map(x, y);
         map = calc_vert(vertices, x, y, map);
+        map = calc_line(map, vertices);
 
         print!("{}{}", Hide, Goto(1, 1));
         render(&map);
@@ -76,9 +77,80 @@ fn calc_vert(vertices: [(f32, f32); 3], x: f32, y: f32, mut map: Vec<Vec<char>>)
     map
 }
 
-fn calc_frag() {}
+fn calc_line(mut map: Vec<Vec<char>>, vertices: [(f32, f32); 3]) -> Vec<Vec<char>> {
+    // TODO: refactor this garbage eventually
+    // TODO: entiere thing mirrored in x axis why idk??
+    for coord in 0..vertices.len() {
+        let initial_coord = vertices[coord];
+        let next_coord: (f32, f32);
+        match coord {
+            coord if coord == vertices.len() - 1 => {
+                next_coord = vertices[0];
+            }
+            _ => {
+                next_coord = vertices[coord + 1];
+            }
+        }
+        let mut x_jump: u32 = 0;
+        match x_jump {
+            _a if initial_coord.0 < next_coord.0 => {
+                x_jump = next_coord.0 as u32 - initial_coord.0 as u32;
+            }
+            _ => {
+                x_jump = initial_coord.0 as u32 - next_coord.0 as u32;
+            }
+        }
+        let mut y_jump: u32 = 0;
 
-fn create_tri() {}
+        match y_jump {
+            _a if initial_coord.1 < next_coord.1 => {
+                y_jump = next_coord.1 as u32 - initial_coord.1 as u32;
+            }
+            _ => {
+                y_jump = initial_coord.1 as u32 - next_coord.1 as u32;
+            }
+        }
+        let gradient: f32 = y_jump as f32 / x_jump as f32;
+        let mut pos_coord: (f32, f32) = initial_coord;
+
+        // will refactor later into less disgusting i swear
+        if initial_coord.1 < next_coord.1 && initial_coord.0 > next_coord.0 {
+            for _i in 0..(x_jump - 2) as u32 {
+                pos_coord = (pos_coord.0 - 1.0, pos_coord.1 + gradient as f32);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        } else if initial_coord.0 < next_coord.0 && initial_coord.1 > next_coord.1 {
+            for _i in 0..(x_jump - 2) as u32 {
+                pos_coord = (pos_coord.0 + 1.0, pos_coord.1 - gradient);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        } else if initial_coord.1 > next_coord.1 && initial_coord.0 > next_coord.0 {
+            for _i in 0..(x_jump - 2) as u32 {
+                pos_coord = (pos_coord.0 - 1.0, pos_coord.1 - gradient as f32);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        } else if initial_coord.1 < next_coord.1 && initial_coord.0 > initial_coord.0 {
+            for _i in 0..(x_jump - 2) as u32 {
+                pos_coord = (pos_coord.0 + gradient, pos_coord.1 + 1.0);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        } else if initial_coord.1 == next_coord.1 {
+            for _i in 0..(x_jump - 2) as u32 {
+                pos_coord = (pos_coord.0 + 1.0, pos_coord.1);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        } else if initial_coord.0 == next_coord.0 {
+            for _i in 0..(y_jump - 2) as u32 {
+                pos_coord = (pos_coord.0, pos_coord.1 - 1.0);
+                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
+            }
+        }
+    }
+
+    map
+}
+
+fn calc_fill() {}
 
 fn render(map: &Vec<Vec<char>>) {
     for row in map {
