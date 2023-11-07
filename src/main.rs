fd8344314fc376efd119180459aea28e16bd2cd9
CmrCrabs <zayaanazam1234@hotmail.com>
2023-11-07 15:29:34 +0000

    works i love algorithms

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::{thread::sleep, time::Duration};
+use termion::cursor::{Goto, Hide};
 use termion::terminal_size;
-use termion::{
-    clear,
-    cursor::{Goto, Hide},
-};
 
 fn main() {
     let initial_vertices: [(f32, f32); 3] = [(0.5, 1.0), (0.0, 0.0), (1.0, 0.0)];
-    // let initial_vertices: [(f32, f32); 3] = [(0.5, 0.8), (0.2, 0.2), (0.8, 0.2)];
+    let initial_vertices: [(f32, f32); 3] = [(0.5, 0.8), (0.2, 0.2), (0.8, 0.2)];
 
     let (mut x, mut y);
     let mut vertices: [(f32, f32); 3];
@@ -18,17 +15,12 @@ fn main() {
         (x, y) = get_dimensions();
         vertices = convert_to_clipspace(x, y, initial_vertices);
         map = create_map(x, y);
-        map = calc_vert(vertices, x, y, map);
-        map = calc_line(map, vertices);
-        // map = calc_fill(map);
+        map = rasterise(map, vertices);
 
         print!("{}{}", Hide, Goto(1, 1));
         render(&map);
     }
 }
-
-// CURRENT PROBLEMS
-// final branch of match staement does not work
 
 fn get_dimensions() -> (f32, f32) {
     let (f, r) = terminal_size().unwrap();
@@ -53,137 +45,33 @@ fn create_map(x: f32, y: f32) -> Vec<Vec<char>> {
     map
 }
 
-fn calc_vert(vertices: [(f32, f32); 3], x: f32, y: f32, mut map: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    for coord in vertices {
-        let coord_real: (u32, u32) = (coord.0 as u32, coord.1 as u32);
+fn rasterise(mut map: Vec<Vec<char>>, vertices: [(f32, f32); 3]) -> Vec<Vec<char>> {
+    for l in 0..map.len() - 1 {
+        for p in 0..map[l].len() - 1 {
+            let mut count = 0;
 
-        match coord_real {
-            (a, b) if a == x as u32 && b == y as u32 => {
-                map[y as usize - (coord.1 as usize + 1)][coord.0 as usize + 1] = 'X';
-            }
-            (0, 0) => {
-                map[(y as usize - 1) - coord.1 as usize][coord.0 as usize] = 'X';
-            }
-            (0, _) => {
-                map[(y as usize - 1) - coord.1 as usize][coord.0 as usize] = 'X';
-            }
-            (_, 0) => {
-                map[y as usize - coord.1 as usize - 1][coord.0 as usize - 1] = 'X';
-            }
-            (_, _) => {
-                map[(y as usize) - coord.1 as usize][coord.0 as usize] = 'X';
-            }
-        }
-    }
+            for i in 0..vertices.len() {
+                let v_0: (f32, f32, f32) = (vertices[i].0, vertices[i].1, 0.0);
 
-    map
-}
-
-fn calc_line(mut map: Vec<Vec<char>>, vertices: [(f32, f32); 3]) -> Vec<Vec<char>> {
-    // TODO: refactor this garbage eventually
-    // TODO: entiere thing mirrored in x axis why idk??
-    for coord in 0..vertices.len() {
-        let initial_coord = vertices[coord];
-        let next_coord: (f32, f32);
-        match coord {
-            coord if coord == vertices.len() - 1 => {
-                next_coord = vertices[0];
-            }
-            _ => {
-                next_coord = vertices[coord + 1];
-            }
-        }
-        let mut x_jump: u32 = 0;
-        match x_jump {
-            _a if initial_coord.0 < next_coord.0 => {
-                x_jump = next_coord.0 as u32 - initial_coord.0 as u32;
-            }
-            _ => {
-                x_jump = initial_coord.0 as u32 - next_coord.0 as u32;
-            }
-        }
-        let mut y_jump: u32 = 0;
-
-        match y_jump {
-            _a if initial_coord.1 < next_coord.1 => {
-                y_jump = next_coord.1 as u32 - initial_coord.1 as u32;
-            }
-            _ => {
-                y_jump = initial_coord.1 as u32 - next_coord.1 as u32;
-            }
-        }
-        let gradient: f32 = y_jump as f32 / x_jump as f32;
-        let mut pos_coord: (f32, f32) = initial_coord;
-
-        if initial_coord.1 < next_coord.1 && initial_coord.0 > next_coord.0 {
-            for _i in 0..(x_jump - 2) as u32 {
-                pos_coord = (pos_coord.0 - 1.0, pos_coord.1 + gradient as f32);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        } else if initial_coord.0 < next_coord.0 && initial_coord.1 > next_coord.1 {
-            for _i in 0..(x_jump - 2) as u32 {
-                pos_coord = (pos_coord.0 + 1.0, pos_coord.1 - gradient);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        } else if initial_coord.1 > next_coord.1 && initial_coord.0 > next_coord.0 {
-            for _i in 0..(x_jump - 2) as u32 {
-                pos_coord = (pos_coord.0 - 1.0, pos_coord.1 - gradient as f32);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        } else if initial_coord.1 < next_coord.1 && initial_coord.0 > initial_coord.0 {
-            for _i in 0..(x_jump - 2) as u32 {
-                pos_coord = (pos_coord.0 + gradient, pos_coord.1 + 1.0);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        } else if initial_coord.1 == next_coord.1 {
-            for _i in 0..(x_jump - 2) as u32 {
-                pos_coord = (pos_coord.0 + 1.0, pos_coord.1);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        } else if initial_coord.0 == next_coord.0 {
-            for _i in 0..(y_jump - 2) as u32 {
-                pos_coord = (pos_coord.0, pos_coord.1 - 1.0);
-                map[pos_coord.1 as usize][pos_coord.0 as usize] = 'X';
-            }
-        }
-    }
-
-    map
-}
-
-fn calc_fillreal(mut map: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut real = false;
-
-    for line in 0..(map.len() - 1) {
-        for pixel in 0..(map[line].len() - 1) {
-            if map[line][pixel] == 'X' {
-                real ^= true;
-            }
-            if real == true && map[line][pixel] != 'X' {
-                map[line][pixel] = 'X'
-            }
-        }
-    }
-    map
-}
-
-fn calc_fill(mut map: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut real = false;
-
-    for line in 0..(map.len() - 1) {
-        for pixel in 0..(map[line].len() - 1) {
-            match map[line][pixel] {
-                ' ' if real == false => {}
-                ' ' if real == true => {
-                    map[line][pixel] = 'X';
+                let v_1: (f32, f32, f32);
+                if vertices[i] == vertices[vertices.len() - 1] {
+                    v_1 = (vertices[0].0, vertices[0].1, 0.0);
+                } else {
+                    v_1 = (vertices[i + 1].0, vertices[i + 1].1, 0.0);
                 }
-                'X' if real == false => {
-                    real = true;
+
+                let p: (f32, f32, f32) = (p as f32, l as f32, 0.0);
+
+                match ((v_1.0 - v_0.0) * (p.1 - v_0.1)) - ((v_1.1 - v_0.1) * (p.0 - v_0.0)) {
+                    real if real > 0.0 => {
+                        count = count + 1;
+                    }
+                    _ => {}
                 }
-                'X' if real == true => {
-                    real = false;
-                }
-                _ => {}
+            }
+
+            if count == 3 {
+                map[l][p] = 'X';
             }
         }
     }
